56ad7a94371782526df866dbc1b1aba3c75dd442
brusolis228@gmail.com <brusolis228@gmail.com>
2024-11-16 20:41:56 +0300

    dd

--- services/command/app/repositories/tokens_repo.py
+++ services/command/app/repositories/tokens_repo.py
@@ -5,3 +5,10 @@ from app.repositories.base_repo import BaseRepository
 
 class TokensRepository(BaseRepository[Tokens]):
     model: Type[Tokens] = Tokens
+from typing import Type
+from app.domain.tokens import Tokens
+from app.repositories.base_repo import BaseRepository
+
+
+class TokensRepository(BaseRepository[Tokens]):
+    model: Type[Tokens] = Tokens
